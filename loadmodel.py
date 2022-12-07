67afe812b6a605877fff9a634103417afc08a155
Neiru <barajasalonsoc@gmail.com>
2022-12-07 15:00:33 -0600

    la reconcha

--- loadmodel.py
+++ loadmodel.py
@@ -1,22 +1,57 @@
-import cv2
+import cv2 as cv
+from torchvision.models.detection import keypointrcnn_resnet50_fpn, KeypointRCNN_ResNet50_FPN_Weights
 import numpy as np
- 
-onnx_model_path = "./keypoint_rcnn.onnx" 
-sample_image = ""
- 
-#The Magic:
-net =  cv2.dnn.readNetFromONNX(onnx_model_path) 
-image = cv2.imread(sample_image)
-blob = cv2.dnn.blobFromImage(image, 1.0 / 255, (224, 224),(0, 0, 0), swapRB=True, crop=False)
-net.setInput(blob)
-preds = net.forward()
-biggest_pred_index = np.array(preds)[0].argmax()
-print ("Predicted class:",biggest_pred_index)
- 
-import requests
- 
-LABELS_URL = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
-labels = {int(key):value for (key, value)
-          in requests.get(LABELS_URL).json().items()}
- 
-print("The class",biggest_pred_index, "correspond to", labels[biggest_pred_index])
+from torchvision import transforms as T
+
+
+
+
+torch_model = keypointrcnn_resnet50_fpn(weights=KeypointRCNN_ResNet50_FPN_Weights.DEFAULT)
+
+torch_model.eval()
+
+
+img = cv.imread("data/0000001.jpg")
+
+transform = T.Compose([T.ToTensor()])
+img_t = transform(img)
+
+output = torch_model([img_t])[0]
+
+print(output)
+
+
+import matplotlib.pyplot as plt
+
+def draw_keypoints_per_person(img, all_keypoints, all_scores, confs, keypoint_threshold=2, conf_threshold=0.9):
+    # initialize a set of colors from the rainbow spectrum
+    cmap = plt.get_cmap('rainbow')
+    # create a copy of the image
+    img_copy = img.copy()
+    # pick a set of N color-ids from the spectrum
+    color_id = np.arange(1,255, 255//len(all_keypoints)).tolist()[::-1]
+    # iterate for every person detected
+    for person_id in range(len(all_keypoints)):
+      # check the confidence score of the detected person
+      if confs[person_id]>conf_threshold:
+        # grab the keypoint-locations for the detected person
+        keypoints = all_keypoints[person_id, ...]
+        # grab the keypoint-scores for the keypoints
+        scores = all_scores[person_id, ...]
+        # iterate for every keypoint-score
+        for kp in range(len(scores)):
+            # check the confidence score of detected keypoint
+            if scores[kp]>keypoint_threshold:
+                # convert the keypoint float-array to a python-list of integers
+                keypoint = tuple(map(int, keypoints[kp, :2].detach().numpy().tolist()))
+                # pick the color at the specific color-id
+                color = tuple(np.asarray(cmap(color_id[person_id])[:-1])*255)
+                # draw a circle over the keypoint location
+                cv.circle(img_copy, keypoint, 30, color, -1)
+    return img_copy
+
+
+keypoints_img = draw_keypoints_per_person(img, output["keypoints"], output["keypoints_scores"], output["scores"], keypoint_threshold=2)
+
+plt.imshow(keypoints_img)
+plt.show()
--- src/utils/cnnutils.py
+++ src/utils/cnnutils.py
@@ -1,13 +1,47 @@
-import torchvision
-import torch
-from torchvision.models.detection import KeypointRCNN_ResNet50_FPN_Weights
+import io
+import numpy as np
 
-def saveONNX():
-    model = torchvision.models.detection.keypointrcnn_resnet50_fpn(weights=KeypointRCNN_ResNet50_FPN_Weights.DEFAULT)
-    model.eval()
-    x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
-    predictions = model(x)
+from torch import nn
 
-    # optionally, if you want to export the model to ONNX:
-    torch.onnx.export(model, x, "keypoint_rcnn.onnx", opset_version = 11, verbose=True)
+import torch.onnx
 
+import torch.nn as nn
+import torch.nn.init as init
+
+from torchvision.models.detection import keypointrcnn_resnet50_fpn, KeypointRCNN_ResNet50_FPN_Weights
+
+
+batch_size = 1
+
+torch_model = keypointrcnn_resnet50_fpn(weights=KeypointRCNN_ResNet50_FPN_Weights.DEFAULT)
+
+torch_model.eval()
+
+# Input to the model
+x = torch.randn(batch_size, 1, 224, 224, requires_grad=False)
+torch_out = torch_model(x)
+
+# Export the model
+torch.onnx.export(torch_model, x, "keypoint_rcnn.onnx", opset_version = 11)
+
+
+import onnx
+
+onnx_model = onnx.load("keypoint_rcnn.onnx")
+onnx.checker.check_model(onnx_model)
+
+import onnxruntime
+
+ort_session = onnxruntime.InferenceSession("keypoint_rcnn.onnx")
+
+def to_numpy(tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+# compute ONNX Runtime output prediction
+ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
+ort_outs = ort_session.run(None, ort_inputs)
+
+# compare ONNX Runtime and PyTorch results
+np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
+
+print("Exported model has been tested with ONNXRuntime, and the result looks good!")
